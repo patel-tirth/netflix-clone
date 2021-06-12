a65d4a0585b0c3471f8f51241cc2fe39f319b1db
tpatel79 <tpatel79@uic.edu>
2021-06-12 15:13:10 -0400

    connected to heroku database

---
+++ includes/config.php
@@ -0,0 +1,28 @@
+<?php
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to D
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+// ob_start();
+// session_start();
+
+// date_default_timezone_set("America/Chicago");
+
+// try{
+//     $con = new PDO("mysql:dbname=netlix;host=us-cdbr-east-04.cleardb.com","bf07d4fbc85d61","b754e611");
+//     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+// }
+
+// catch(PDOException $e){
+//     exit("Connection failed: " . $e->getMessage());
+// }
+
+?>
