11ab10ee6d344a01c0c1c992fdcad2e878c57d7f
tpatel79 <tpatel79@uic.edu>
2021-06-12 23:31:51 -0400

    updated with gitignore

--- register.php
+++ register.php
@@ -3,8 +3,6 @@ require_once("includes/classes/FormSanitizer.php");
 require_once("includes/config.php");
 require_once("includes/classes/Account.php");
 require_once("includes/classes/Constants.php");
-
-
     $account = new Account($con);
 
     if(isset($_POST["submitButton"])){
